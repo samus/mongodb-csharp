d18bfe28bcdbbdfb8191ce8bebb23e613c3f8921
Samuel Corder <samus@Churchill.local>
2010-01-26 01:47:17 -0500

    Both sequential and random writes in the GridFileStream seem to be working well now.

--- MongoDB.Driver.GridFS.Tests/GridFileStreamTest.cs
+++ MongoDB.Driver.GridFS.Tests/GridFileStreamTest.cs
@@ -12,7 +12,7 @@ namespace MongoDB.Driver.GridFS
     {
         Mongo db = new Mongo();
         GridFile fs;
-        
+
         String filesystem = "gfstream";
         [Test]
         public void TestWrite(){
@@ -20,17 +20,17 @@ namespace MongoDB.Driver.GridFS
             Object id = gfs.GridFileInfo.Id;
 
             for(byte b = (byte)0; b < 128; b++){
-                gfs.WriteByte(b);    
+                gfs.WriteByte(b);
             }
             gfs.Close();
-            
+
             Assert.AreEqual(1, CountChunks(id));
             Document chunk = GrabChunk(id, 0);
             Binary bin = (Binary)chunk["data"];
             Assert.AreEqual(127, bin.Bytes[127]);
-            Assert.AreEqual(0, bin.Bytes[0]);            
+            Assert.AreEqual(0, bin.Bytes[0]);
         }
-        
+
         [Test]
         public void TestWriteMultipleBytes(){
             GridFileStream gfs = fs.Create("multiplebytes.txt");
@@ -40,84 +40,106 @@ namespace MongoDB.Driver.GridFS
                 gfs.Write(BitConverter.GetBytes(x),0,4);
             }
             gfs.Close();
-            
+
             Assert.AreEqual(1, CountChunks(id));
-        }        
-        
+        }
+
         [Test]
         public void TestWriteTo3Chunks(){
             GridFileStream gfs = fs.Create("largewrite.txt");
-            
+
             Object id = gfs.GridFileInfo.Id;
             int chunks = 3;
-            Byte[] buff = new byte[(256 * 1024) * chunks]; //intentionally bigger than default buffer size.
-            for(int i = 0; i < buff.Length; i++){
-                buff[i] = (byte)(i % 128);
-            }
+            int buffsize = 256 * 1024 * chunks;
+            Byte[] buff = CreateBuffer(buffsize, (byte)6);
             gfs.Write(buff,0,buff.Length);
             Assert.AreEqual(buff.Length, gfs.Position);
             gfs.Close();
-            Assert.AreEqual(chunks, db["tests"][filesystem + ".chunks"].Count(new Document().Append("files_id", id)));
+            Assert.AreEqual(chunks, CountChunks(id));
         }
-        
+
         [Test]
         public void TestNonSequentialWriteToOneChunk(){
             string filename = "nonsequential1.txt";
             GridFileStream gfs = fs.Create(filename);
             Object id = gfs.GridFileInfo.Id;
             int chunksize = gfs.GridFileInfo.ChunkSize;
-            
+
             gfs.Seek(chunksize/2, SeekOrigin.Begin);
             byte[] two = new byte[]{2};
             for(int i = chunksize; i > chunksize/2; i--){
                 gfs.Write(two, 0, 1);
             }
-            
+
             gfs.Seek(0, SeekOrigin.Begin);
             byte[] one = new byte[]{1};
             for(int i = 0; i < chunksize/2; i++){
                 gfs.Write(one, 0, 1);
-            }            
+            }
             gfs.Close();
-            
+
             Assert.AreEqual(1, CountChunks(id));
             Document chunk = GrabChunk(id, 0);
             Binary b = (Binary)chunk["data"];
-            Assert.AreEqual(1, b.Bytes[chunksize-1]);
-            Assert.AreEqual(1, b.Bytes[chunksize/2]);
-            Assert.AreEqual(2, b.Bytes[chunksize/2 -1]);
-            Assert.AreEqual(2, b.Bytes[0]);
+            Assert.AreEqual(2, b.Bytes[chunksize-1]);
+            Assert.AreEqual(2, b.Bytes[chunksize/2]);
+            Assert.AreEqual(1, b.Bytes[chunksize/2 -1]);
+            Assert.AreEqual(1, b.Bytes[0]);
         }
-        
+        [Test]
+        public void TestNonSequentialWriteToPartialChunk(){
+            string filename = "nonsequentialpartial.txt";
+            GridFileStream gfs = fs.Create(filename);
+            Object id = gfs.GridFileInfo.Id;
+            int chunksize = gfs.GridFileInfo.ChunkSize;
+            gfs.Write(BitConverter.GetBytes(0),0,4);
+            Assert.AreEqual(4, gfs.Position);
+            gfs.Write(BitConverter.GetBytes(7),0,4);
+            Assert.AreEqual(8, gfs.Position);
+            gfs.Seek(0, SeekOrigin.Begin);
+            gfs.Write(BitConverter.GetBytes(15),0,4);
+            gfs.Close();
+            
+            Document chunk = GrabChunk(id, 0);
+            Binary b = (Binary)chunk["data"];
+            Assert.AreEqual(8,b.Bytes.Length);
+            
+        }
+
         [Test]
         public void TestNonSequentialWriteToTwoChunks(){
             GridFileStream gfs = fs.Create("nonsequential2.txt");
-            
+
             Object id = gfs.GridFileInfo.Id;
             int chunks = 3;
             int buffsize = 256 * 1024;
             for(int c = 0; c < chunks; c++){
-                Byte[] buff = new byte[buffsize];
-                for(int i = 0; i < buff.Length; i++){
-                    buff[i] = (byte)(c);
+            	Byte[] buff = CreateBuffer(buffsize, (byte)c);
+                if(c == 2){
+                	gfs.Seek(0, SeekOrigin.Begin); //On last chunk seek to start.
                 }
-                if(c == 2) gfs.Seek(0, SeekOrigin.Begin); //On last chunk seek to start.
-                gfs.Write(buff,0,buff.Length);                
+                gfs.Write(buff,0,buff.Length);
             }
             Assert.AreEqual(buffsize, gfs.Position, "Position is incorrect");
             gfs.Close();
             Assert.AreEqual(chunks - 1, CountChunks(id));
-            //TODO Assert some content checks.
-        }        
-        
+            Document chunk = GrabChunk(id, 0);
+            Binary b = (Binary)chunk["data"];
+            Assert.AreEqual(2, b.Bytes[buffsize-1]);
+            Assert.AreEqual(2, b.Bytes[0]);
+            chunk = GrabChunk(id, 1);
+            b = (Binary)chunk["data"];
+            Assert.AreEqual(1, b.Bytes[buffsize-1]);
+        }
+
         #region File API compatibility
-        
+
         [Test]
         public void TestSeekingBeyondEOF(){
             int buffsize = 256;
             FileStream gfs = File.Create("seektest.txt",buffsize);//,FileOptions.DeleteOnClose);
             int chunks = 3;
-            //int buffsize = 256 * 1024;  
+            //int buffsize = 256 * 1024;
             for(int c = 0; c < chunks; c++){
                 Byte[] buff = new byte[buffsize];
                 for(int i = 0; i < buff.Length; i++){
@@ -129,45 +151,53 @@ namespace MongoDB.Driver.GridFS
             }
             gfs.Seek(50,SeekOrigin.End);
             gfs.Write(new byte[]{5},0,1);
-            
-            
+
+
         }
-        
+
         #endregion
-        
-        
+
+
         [TestFixtureSetUp]
         public void Init(){
             db.Connect();
             fs = new GridFile(db["tests"], filesystem);
             CleanDB(); //Run here instead of at the end so that the db can be examined after a run.
         }
-        
+
         [TestFixtureTearDown]
         public void Dispose(){
             db.Disconnect();
         }
-        
+
         protected void CleanDB(){
             //Any collections that we might want to delete before the tests run should be done here.
             DropGridFileSystem(filesystem);
         }
-        
+
         protected void DropGridFileSystem(string filesystem){
             try{
                 db["tests"].MetaData.DropCollection(filesystem + ".files");
                 db["tests"].MetaData.DropCollection(filesystem + ".chunks");
             }catch(MongoCommandException){}//if it fails it is because the collection isn't there to start with.
-            
+
         }
-        
+
         protected long CountChunks(Object fileid){
             return db["tests"][filesystem + ".chunks"].Count(new Document().Append("files_id", fileid));
         }
-        
+
         protected Document GrabChunk(Object fileid, int chunk){
             return db["tests"][filesystem + ".chunks"].FindOne(new Document().Append("files_id", fileid).Append("n", chunk));
         }
         
+        protected byte[] CreateBuffer(int size, byte fill){
+            Byte[] buff = new byte[size];
+            for(int i = 0; i < buff.Length; i++){
+                buff[i] = fill;
+            }
+            return buff;
+        }
+
     }
 }
--- MongoDB.Driver.GridFS/GridFileStream.cs
+++ MongoDB.Driver.GridFS/GridFileStream.cs
@@ -21,8 +21,13 @@ namespace MongoDB.Driver.GridFS
         private IMongoCollection chunks;
         private Document chunk;
         private bool chunkDirty;
+        private long chunkLower = -1;
+        private long chunkUpper = -1;
+
         private byte[] buffer;
-        private int buffWritten;
+        private byte[] blankBuffer;
+        private int buffPosition;
+        private int highestBuffPosition;
                 
         #region Properties
         private GridFileInfo gridFileInfo;       
@@ -58,7 +63,7 @@ namespace MongoDB.Driver.GridFS
                 return position;
             }
             set {
-                position = value;
+                this.Seek(value, SeekOrigin.Begin);
             }
         }
         #endregion
@@ -80,6 +85,8 @@ namespace MongoDB.Driver.GridFS
             this.files = files;
             this.chunks = chunks;
             this.buffer = new byte[gridFileInfo.ChunkSize];
+            this.blankBuffer = new byte[gridFileInfo.ChunkSize];
+            this.MoveTo(0);
         }
         
         /// <summary>
@@ -96,42 +103,28 @@ namespace MongoDB.Driver.GridFS
         /// </param>
         public override void Write(byte[] array, int offset, int count){
             ValidateWriteState(array,offset,count);
-            
-            //Baby steps.  First write to the first chunk.            
+                     
             int bytesLeftToWrite = count;
             while(bytesLeftToWrite > 0){
-                EnsureWriteChunkLoaded();
-                int buffAvailable = buffer.Length - buffWritten;
+                int buffAvailable = buffer.Length - buffPosition;
                 int writeCount = 0;
                 if(buffAvailable > bytesLeftToWrite){
                     writeCount = bytesLeftToWrite;
                 }else{
                     writeCount = buffAvailable;
                 }
-                Array.Copy(array,offset,buffer,buffWritten,writeCount);
+                Array.Copy(array,offset,buffer,buffPosition,writeCount);
                 chunkDirty = true;
-                buffWritten += writeCount;
-                position += writeCount;
+                buffPosition += writeCount;
                 bytesLeftToWrite -= writeCount;
-                if(buffWritten >= buffer.Length){
-                    Flush();
-                }
-            }
-        }
-
-        private void EnsureWriteChunkLoaded(){
-            int chunknum = (int)Math.Floor((double)(this.position / this.gridFileInfo.ChunkSize));
-            if(chunk == null){
-                chunk = new Document().Append("files_id", this.GridFileInfo.Id).Append("n",chunknum);
-                buffWritten = 0;
-                chunkDirty = false;
+                MoveTo(position + writeCount);
             }
         }
         
         public override void Flush(){
             if(chunkDirty == false) return;
-            byte[] data = new byte[buffWritten];
-            Array.Copy(buffer,data,buffWritten);
+            byte[] data = new byte[highestBuffPosition];
+            Array.Copy(buffer,data,highestBuffPosition);
             
             chunk["data"] = new Binary(data);
             
@@ -140,61 +133,95 @@ namespace MongoDB.Driver.GridFS
             }else{
                 chunks.Insert(chunk);
             }
-            if(buffWritten >= this.GridFileInfo.ChunkSize){
-                chunk = null;
-                EnsureWriteChunkLoaded();
-            }
         }
         
         private void ValidateWriteState(byte[] array, int offset, int count){
             if (array == null){
                 throw new ArgumentNullException("array", new Exception("array is null"));
-            }
-            else if (offset < 0){
+            }else if (offset < 0){
                 throw new ArgumentOutOfRangeException("offset", new Exception("offset is negative"));
-            }
-            else if (count < 0){
+            }else if (count < 0){
                 throw new ArgumentOutOfRangeException("count",new Exception("count is negative"));
-            }
-            else if ((array.Length - offset) < count){
+            }else if ((array.Length - offset) < count){
                 throw new MongoGridFSException("Invalid count argument", gridFileInfo.FileName, null);
-            }
-            else if (!canWrite){
-                throw new MongoGridFSException("Writing to this file is not supported", gridFileInfo.FileName, null);
+            }else if (!canWrite){
+                throw new MongoGridFSException("Writing to this file is not allowed", gridFileInfo.FileName, null);
             }
         }
         
         public override long Seek(long offset, SeekOrigin origin){
-            if ((origin < SeekOrigin.Begin) || (origin > SeekOrigin.End))
-            {
+            if ((origin < SeekOrigin.Begin) || (origin > SeekOrigin.End)){
                 throw new ArgumentException("Invalid Seek Origin");
             }
             
-            switch (origin)
-            {
+            switch (origin){
                 case SeekOrigin.Begin:
                     if (offset < 0){
                         throw new ArgumentException("Attempted seeking before the begining of the stream");
+                    }else{
+                        MoveTo(offset);
                     }
-                    else
-                        position = offset;
                     break;
-
                 case SeekOrigin.Current:
-                    position += offset;
+                    MoveTo(position + offset);
                     break;
-
                 case SeekOrigin.End:
-                    if (offset <= 0)
-                    {
+                    if (offset <= 0){
                         throw new ArgumentException("Attempted seeking after the end of the stream");
                     }
-                    position = this.Length - offset;
+                    MoveTo(this.Length - offset);
                     break;                  
             }
             return position; 
         }
-        
+
+
+        /// <summary>
+        /// Moves the current position to the new position.  If this causes a new chunk to need to be loaded it will take
+        /// care of flushing the buffer and loading a new chunk. 
+        /// </summary>
+        /// <param name="position">
+        /// A <see cref="System.Int32"/> designating where to go to.
+        /// </param>
+        private void MoveTo(long position){
+            this.position = position;
+            int chunkSize = this.gridFileInfo.ChunkSize;
+            bool chunkInRange = (chunk != null && position >= chunkLower && position < chunkUpper);
+            if(chunkInRange == false){
+            	if(chunk != null  && chunkDirty){
+            		highestBuffPosition = Math.Max(highestBuffPosition, buffPosition);
+            		this.Flush();
+            	}
+                int chunknum = (int)Math.Floor((double)(position / chunkSize));
+                Array.Copy(blankBuffer,buffer,buffer.Length);
+                LoadOrCreateChunk(chunknum);
+                chunkDirty = false;
+                chunkLower = chunknum * chunkSize;
+                chunkUpper = chunkLower + chunkSize;
+            }
+            buffPosition = (int)(position % chunkSize);
+        	highestBuffPosition = Math.Max(highestBuffPosition, buffPosition);
+            
+        }
+
+        /// <summary>
+        /// Loads a chunk from the chunks collection if it exists.  Otherwise it creates a blank chunk Document.
+        /// </summary>
+        /// <param name="num"></param>
+        private void LoadOrCreateChunk(int num){
+        	Object fid = this.GridFileInfo.Id;
+        	Document spec = new Document().Append("files_id", fid).Append("n",num);
+        	chunk = this.chunks.FindOne(spec);
+        	if(chunk == null) {
+        		chunk = spec;
+        		highestBuffPosition = 0;
+        	}else{
+        		Binary b = (Binary)chunk["data"];
+        		highestBuffPosition = b.Bytes.Length;
+        		Array.Copy(b.Bytes,buffer, highestBuffPosition);
+        	}
+        }
+
         public override void SetLength(long value){
             throw new NotImplementedException();
         }
