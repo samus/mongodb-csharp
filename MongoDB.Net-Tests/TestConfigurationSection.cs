7810d596f5837becb19922a62be47b5838f57282
Sedward <sethaedwards@gmail.com>
2010-02-01 15:26:58 -0500

    A Configuration class to read from an app.config or web.config. May need to be expanded on with new features

---
+++ MongoDB.Net-Tests/TestConfigurationSection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using System.Configuration;
+using MongoDB.Driver;
+using MongoDB.Driver.Configuration;
+using System.Text;
+
+namespace MongoDB.Driver.Tests
+{
+    [TestFixture]
+    public class TestConfigurationSection
+    {
+        [Test]
+        public void TestReadConfig(){
+
+            MongoDBConfiguration config = (MongoDBConfiguration)System.Configuration.ConfigurationManager.GetSection("MongoDB");
+            Assert.AreEqual("servername", config.Host);
+            Assert.AreEqual(27017, config.Port);
+            Assert.AreEqual("testcollection", config.Collection);
+       }
+
+    }
+}
---
+++ MongoDBDriver/MongoDBConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+
+namespace MongoDB.Driver.Configuration
+{
+    public class MongoDBConfiguration : ConfigurationSection
+    {
+
+        public MongoDBConfiguration() { }
+
+        [ConfigurationProperty("port", DefaultValue = "27017", IsRequired = true)]
+        [IntegerValidator(MinValue = 1, MaxValue = 65535, ExcludeRange = false)]
+        public int Port
+        {
+            get { return (Int32)this["port"]; }
+            set { this["port"] = value; }
+        }
+
+        [ConfigurationProperty("host", DefaultValue = "localhost", IsRequired = true)]
+         public string Host {
+            get { return (String)this["host"]; }
+            set { this["host"] = value; }
+
+        }
+
+        [ConfigurationProperty("collection", IsRequired = true)]
+        public string Collection{
+            get { return (String)this["collection"]; }
+            set { this["collection"] = value; }
+        }
+
+        [ConfigurationProperty("use_authentication", DefaultValue = false, IsRequired = false)]
+        public bool UseAuthentication{
+            get { return (Boolean)this["use_authentication"]; }
+            set { this["use_authentication"] = value; }
+        }
+
+        [ConfigurationProperty("username", DefaultValue = "", IsRequired = false)]
+        public string Username{
+            get { return (String)this["username"]; }
+            set { this["username"] = value; }
+        }
+
+        [ConfigurationProperty("password", DefaultValue = "", IsRequired = false)]
+        public string Password{
+            get { return (String)this["password"]; }
+            set { this["password"] = value; }
+        }
+
+    }
+
+    /// <summary>
+    /// Not used at the moment but could be used later
+    /// </summary>
+    public class ChildConfigElement : ConfigurationElement
+    {
+        public ChildConfigElement() { }
+    }
+
+}
