c85f968780304871dab1ddc8d026b10c8b1bd46d
Andrew Rondeau <github@andrewrondeau.com>
2010-02-26 20:24:58 -0800

    Replaced Binary.TypeCode.Uuid with true support for Guids

--- MongoDBDriver/Binary.cs
+++ MongoDBDriver/Binary.cs
@@ -8,11 +8,9 @@ namespace MongoDB.Driver
 		public enum TypeCode:byte{
 			Unknown = 0,
 			General = 2,
-
-            // Uuid is now replaced by Binary
-			//Uuid = 3,
-			
-            Md5 = 5,
+			// Uuid is now replaced by Guid
+            //Uuid = 3,
+			Md5 = 5,
 			UserDefined = 80
 		}
 			
