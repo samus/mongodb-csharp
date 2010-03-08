6a4691dc3df05aea218e9b1d6cca39dcdd2ac4dd
Steve Wagner <lanwin@lanwin.de>
2010-03-08 18:20:38 +0100

    Read bson arrays without the use of reflection.

--- MongoDBDriver/Bson/BsonReader.cs
+++ MongoDBDriver/Bson/BsonReader.cs
@@ -49,10 +49,10 @@ namespace MongoDB.Driver.Bson
             }
             byte eoo = reader.ReadByte ();
             position++;
-            if (eoo != (byte)0)
-                throw new System.IO.InvalidDataException ("Document not null terminated");
+            if (eoo != 0)
+                throw new InvalidDataException ("Document not null terminated");
             if (size != position - startpos) {
-                throw new System.IO.InvalidDataException (string.Format ("Should have read {0} bytes from stream but only read {1}", size, (position - startpos)));
+                throw new InvalidDataException (string.Format ("Should have read {0} bytes from stream but only read {1}", size, (position - startpos)));
             }
             return doc;
         }
@@ -262,22 +262,17 @@ namespace MongoDB.Driver.Bson
         }
 
         private IEnumerable ConvertToArray (Document doc){
-            Type genericListType = typeof(List<>);
-            Type arrayType = GetTypeForIEnumerable(doc);
-            Type listType = genericListType.MakeGenericType(arrayType);
+            var genericListType = typeof(List<>);
+            var arrayType = GetTypeForIEnumerable(doc);
+            var listType = genericListType.MakeGenericType(arrayType);
+
+            var list = (IList)Activator.CreateInstance(listType);
             
-            Object ret = Activator.CreateInstance(listType);
-            BindingFlags FlagsAttribute = BindingFlags.Instance | 
-                                          BindingFlags.InvokeMethod | 
-                                          BindingFlags.Public;
-            
-            int idx = 0;
             foreach (String key in doc.Keys) {
-                //ret.Add((T)doc[key]);
-                listType.InvokeMember("Add", FlagsAttribute, null, ret, new Object[]{doc[key]});
-                //idx++;
+                list.Add(doc[key]);
             }
-            return (IEnumerable)ret;
+
+            return list;
         }
     }
 }
