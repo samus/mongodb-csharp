607574e1afd877ffc5f88ee06b74c18bed93a736
Steve Wagner <lanwin@lanwin.de>
2010-03-19 11:01:54 +0100

    Fix App.config in main test project. Use main test App.config also for Linq and GridFs tests.

--- MongoDB.Linq.Tests/AppSettingsFactory.cs
+++ MongoDB.Linq.Tests/AppSettingsFactory.cs
@@ -4,20 +4,14 @@ using MongoDB.Driver.Connections;
 
 namespace MongoDB.Linq.Tests {
     public static class AppSettingsFactory {
-
-        public static string Host { get { return ConfigurationManager.AppSettings["mongo.host"]; } }
-        public static int Port { get { return int.Parse(ConfigurationManager.AppSettings["mongo.port"]); } }
-
         public static Mongo CreateMongo() {
-            var builder = new MongoConnectionStringBuilder();
-            builder.AddServer(Host,Port);
-            return new Mongo(builder.ToString());
+            var connectionString = ConfigurationManager.AppSettings["tests"];
+            return new Mongo(connectionString);
         }
 
         public static Connection CreateConnection() {
-            var builder = new MongoConnectionStringBuilder();
-            builder.AddServer(Host, Port);
-            return ConnectionFactory.GetConnection(builder.ToString());
+            var connectionString = ConfigurationManager.AppSettings["tests"];
+            return ConnectionFactory.GetConnection(connectionString);
         }
     }
 }
