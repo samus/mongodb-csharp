5a84eee6c92869c6a54f68fd0b63fbdfe8e81aa8
Steve Wagner <lanwin@lanwin.de>
2010-03-19 13:17:02 +0100

    Cleanup DBRef.

--- MongoDBDriver/DBRef.cs
+++ MongoDBDriver/DBRef.cs
@@ -3,115 +3,159 @@ using System;
 namespace MongoDB.Driver
 {
     /// <summary>
-    /// Native type that maps to a database reference.  Use Database.FollowReference(DBRef) to retrieve the document
-    /// that it refers to.
+    ///   Native type that maps to a database reference.  Use Database.FollowReference(DBRef) to retrieve the document
+    ///   that it refers to.
     /// </summary>
-    /// <remarks>DBRefs are just a specification for a specially formatted Document.  At this time the database
-    /// does no special handling of them. Any referential integrity must be maintained by the application 
-    /// not the database.
+    /// <remarks>
+    ///   DBRefs are just a specification for a specially formatted Document.  At this time the database
+    ///   does no special handling of them. Any referential integrity must be maintained by the application
+    ///   not the database.
     /// </remarks>
     public class DBRef
     {
-        public const string RefName = "$ref";
         public const string IdName = "$id";
         public const string MetaName = "metadata";
-        
-        private Document document;
+        public const string RefName = "$ref";
+
+        private readonly Document _document;
+        private string _collectionName;
+        private object _id;
+        private Document _metadata;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "DBRef" /> class.
+        /// </summary>
+        public DBRef(){
+            _document = new Document();
+        }
+
+        /// <summary>
+        ///   Constructs a DBRef from a document that matches the DBref specification.
+        /// </summary>
+        public DBRef(Document document){
+            if(IsDocumentDBRef(document) == false)
+                throw new ArgumentException("Document is not a valid DBRef");
+            _collectionName = (String)document[RefName];
+            _id = document[IdName];
+            this._document = document;
+            if(document.Contains("metadata"))
+                MetaData = (Document)document["metadata"];
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DBRef"/> class.
         /// </summary>
-        public DBRef(){
-            document = new Document();
-        }
-
-        private string collectionName;
-        /// <summary>
-        /// The name of the collection the referenced document is in. 
-        /// </summary>
-        public string CollectionName {
-            get { return collectionName; }
-            set {
-                collectionName = value; 
-                document[RefName] = value;
-            }
-        }
-
-        private object id;
-        /// <summary>
-        /// Object value of the id.  It isn't an Oid because document ids are not required to be oids.
-        /// </summary>
-        public object Id {
-            get { return id; }
-            set {
-                id = value; 
-                document[IdName] = value;
-            }
-        }
-
-        private Document metadata;
-        /// <summary>
-        /// An extension to the spec that allows storing of arbitrary data about a reference.  
-        /// </summary>
-        /// <remarks>This is a non-standard feature.
-        /// </remarks>
-        public Document MetaData {
-            get{return metadata; }
-            set{
-                metadata = value;
-                document[MetaName] = value;
-            }
-        }
-
-        /// <summary>
-        /// Constructs a DBRef from a document that matches the DBref specification.
-        /// </summary>
-        public DBRef(Document document){
-            if(IsDocumentDBRef(document) == false) throw new ArgumentException("Document is not a valid DBRef");
-            collectionName = (String)document[RefName];
-            id = document[IdName];
-            this.document = document;
-            if(document.Contains("metadata")) this.MetaData = (Document)document["metadata"];
-        }
-        
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <param name="id">The id.</param>
         public DBRef(string collectionName, object id){
-            document = new Document();
-            this.CollectionName = collectionName;
-            this.Id = id;
-        }       
+            _document = new Document();
+            CollectionName = collectionName;
+            Id = id;
+        }
 
+        /// <summary>
+        ///   The name of the collection the referenced document is in.
+        /// </summary>
+        public string CollectionName{
+            get { return _collectionName; }
+            set{
+                _collectionName = value;
+                _document[RefName] = value;
+            }
+        }
+
+        /// <summary>
+        ///   Object value of the id.  It isn't an Oid because document ids are not required to be oids.
+        /// </summary>
+        public object Id{
+            get { return _id; }
+            set{
+                _id = value;
+                _document[IdName] = value;
+            }
+        }
+
+        /// <summary>
+        /// An extension to the spec that allows storing of arbitrary data about a reference.
+        /// </summary>
+        /// <value>The meta data.</value>
+        /// <remarks>
+        /// This is a non-standard feature.
+        /// </remarks>
+        public Document MetaData{
+            get { return _metadata; }
+            set{
+                _metadata = value;
+                _document[MetaName] = value;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="T:System.NullReferenceException">
+        /// The <paramref name="obj"/> parameter is null.
+        /// </exception>
         public override bool Equals(object obj){
             if(obj is DBRef){
-                DBRef comp = (DBRef)obj;
-                return comp.Id.Equals(this.Id) && comp.CollectionName.Equals(this.CollectionName);
+                var comp = (DBRef)obj;
+                return comp.Id.Equals(Id) && comp.CollectionName.Equals(CollectionName);
             }
             return base.Equals(obj);
         }
 
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
         public override int GetHashCode(){
             unchecked{
-                return ((this.collectionName != null ? this.collectionName.GetHashCode() : 0) * 397) ^ (this.id != null ? this.id.GetHashCode() : 0);
+                return ((_collectionName != null ? _collectionName.GetHashCode() : 0)*397) ^ (_id != null ? _id.GetHashCode() : 0);
             }
         }
-        
-        public override string ToString (){
-            return document.ToString();
-        }
-
 
         /// <summary>
-        /// Deprecated.  Use the new DBRef(Document) constructor instead.
+        /// Returns a <see cref="System.String"/> that represents this instance.
         /// </summary>
-        public static DBRef FromDocument(Document doc){
-            return new DBRef(doc);;
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString(){
+            return _document.ToString();
         }
-        
-        public static bool IsDocumentDBRef(Document doc){
-            return doc != null && doc.Contains(RefName) && doc.Contains(IdName);
+
+        /// <summary>
+        ///   Deprecated.  Use the new DBRef(Document) constructor instead.
+        /// </summary>
+        public static DBRef FromDocument(Document document){
+            return new DBRef(document);
+            ;
         }
-        
-        public static explicit operator Document(DBRef d){
-            return d.document;
-        }        
+
+        /// <summary>
+        ///   Determines whether [is document DB ref] [the specified document].
+        /// </summary>
+        /// <param name = "document">The document.</param>
+        /// <returns>
+        ///   <c>true</c> if [is document DB ref] [the specified document]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsDocumentDBRef(Document document){
+            return document != null && document.Contains(RefName) && document.Contains(IdName);
+        }
+
+        /// <summary>
+        /// Performs an explicit conversion from <see cref="MongoDB.Driver.DBRef"/> to <see cref="MongoDB.Driver.Document"/>.
+        /// </summary>
+        /// <param name="dbRef">The db ref.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static explicit operator Document(DBRef dbRef){
+            return dbRef._document;
+        }
     }
 }
