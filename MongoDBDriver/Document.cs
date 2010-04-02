cb23822800b0402d1d2ce985f1bc660df8efbdcc
Samuel Corder <samus@Churchill.local>
2010-04-02 15:28:27 -0400

    Removed tabs from Document.cs

--- MongoDBDriver/Document.cs
+++ MongoDBDriver/Document.cs
@@ -9,52 +9,45 @@ namespace MongoDB.Driver
     /// </summary>
     public class Document : DictionaryBase
     {
-		private bool _dirty;
+        private bool _dirty;
         private List<String> orderedKeys;
         private IComparer<string> keyComparer;
 
 
-        public Document() : this(null)
-        {
+        public Document() : this(null) {
         }
 
-        public Document(IComparer<string> comparer)
-        {
+        public Document(IComparer<string> comparer) {
             orderedKeys = new List<string>();
             keyComparer = comparer;
         }
 
-        public Object this[String key]
-        {
+        public Object this[String key] {
             get { return Dictionary[key]; }
-            set{
-				_dirty = true;
-				Dictionary[key] = value;
-			}
-        }
-
-        public IList<string> Keys
-        {
-            get{
-                if (keyComparer != null && _dirty)
-                    orderedKeys.Sort(keyComparer);
-                return orderedKeys; 
+            set {
+                _dirty = true;
+                Dictionary[key] = value;
             }
         }
 
-        public ICollection Values
-        {
+        public IList<string> Keys {
+            get {
+                if(keyComparer != null && _dirty)
+                    orderedKeys.Sort(keyComparer);
+                return orderedKeys;
+            }
+        }
+
+        public ICollection Values {
             get { return (Dictionary.Values); }
         }
 
-        public void Add(String key, Object value)
-        {
-			_dirty = true;
+        public void Add(String key, Object value) {
+            _dirty = true;
             Dictionary.Add(key, value);
         }
 
-        public Document Append(String key, Object value)
-        {
+        public Document Append(String key, Object value) {
             this.Add(key, value);
             return this;
         }
@@ -62,54 +55,47 @@ namespace MongoDB.Driver
         /// <summary>
         /// Adds an item to the Document at the specified position
         /// </summary>
-        public void Insert(String key, Object value, int index)
-        {
+        public void Insert(String key, Object value, int index) {
             bool alreadyContains = orderedKeys.Contains(key);
-            if (!alreadyContains) orderedKeys.Insert(index, key);
-            else throw new ArgumentException("Key already exists in Document", "key");
-
-            try
-            {
+            if(!alreadyContains)
+                orderedKeys.Insert(index, key);
+            else
+                throw new ArgumentException("Key already exists in Document", "key");
+            
+            try {
                 Dictionary.Add(key, value);
-            }
-            catch
-            {
-                if (!alreadyContains) orderedKeys.Remove(key);
+            } catch {
+                if(!alreadyContains)
+                    orderedKeys.Remove(key);
                 throw;
             }
-			_dirty = true;
+            _dirty = true;
         }
 
-        public Document Prepend(String key, Object value)
-        {
+        public Document Prepend(String key, Object value) {
             this.Insert(key, value, 0);
             return this;
         }
 
-        public Document Update(Document @from)
-        {
-            if (@from == null)
+        public Document Update(Document @from) {
+            if(@from == null)
                 return this;
-            foreach (String key in @from.Keys)
-            {
+            foreach(String key in @from.Keys) {
                 this[key] = @from[key];
             }
             return this;
         }
 
-        public bool Contains(String key)
-        {
+        public bool Contains(String key) {
             return (orderedKeys.Contains(key));
         }
 
-        public void Remove(String key)
-        {
-			_dirty = true;
+        public void Remove(String key) {
+            _dirty = true;
             Dictionary.Remove(key);
         }
 
-        public new void Clear()
-        {
+        public new void Clear() {
             Dictionary.Clear();
         }
 
@@ -117,42 +103,34 @@ namespace MongoDB.Driver
         /// TODO Fix any accidental reordering issues.
         /// </summary>
         /// <param name="dest"></param>
-        public void CopyTo(Document dest)
-        {
-            foreach (String key in orderedKeys)
-            {
-                if (dest.Contains(key))
+        public void CopyTo(Document dest) {
+            foreach(String key in orderedKeys) {
+                if(dest.Contains(key))
                     dest.Remove(key);
                 dest[key] = this[key];
             }
         }
 
-        public override bool Equals(object obj)
-        {
-            if (obj is Document)
-            {
+        public override bool Equals(object obj) {
+            if(obj is Document) {
                 return Equals(obj as Document);
             }
             return base.Equals(obj);
         }
 
-        public bool Equals(Document obj)
-        {
-            if (obj == null)
+        public bool Equals(Document obj) {
+            if(obj == null)
                 return false;
-            if (Keys.Count != obj.Keys.Count)
+            if(Keys.Count != obj.Keys.Count)
                 return false;
             return this.GetHashCode() == obj.GetHashCode();
         }
 
-        public override int GetHashCode()
-        {
+        public override int GetHashCode() {
             int hash = 27;
-            foreach (string key in Keys)
-            {
+            foreach(string key in Keys) {
                 var valueHashCode = GetValueHashCode(this[key]);
-                unchecked
-                {
+                unchecked {
                     hash = (13 * hash) + key.GetHashCode();
                     hash = (13 * hash) + valueHashCode;
                 }
@@ -160,61 +138,51 @@ namespace MongoDB.Driver
             return hash;
         }
 
-        private int GetValueHashCode(object value)
-        {
-            if (value == null)
-            {
+        private int GetValueHashCode(object value) {
+            if(value == null) {
                 return 0;
             }
             return (value is Array) ? GetArrayHashcode((Array)value) : value.GetHashCode();
         }
 
-        private int GetArrayHashcode(Array array)
-        {
+        private int GetArrayHashcode(Array array) {
             var hash = 0;
-            foreach (var value in array)
-            {
+            foreach(var value in array) {
                 var valueHashCode = GetValueHashCode(value);
-                unchecked
-                {
+                unchecked {
                     hash = (13 * hash) + valueHashCode;
                 }
             }
             return hash;
         }
 
-        protected override void OnClear()
-        {
+        protected override void OnClear() {
             orderedKeys.Clear();
             base.OnClear();
         }
 
-        protected override void OnInsertComplete(object key, object value)
-        {
+        protected override void OnInsertComplete(object key, object value) {
             string strKey = key as string;
-            if (!orderedKeys.Contains(strKey))
+            if(!orderedKeys.Contains(strKey))
                 orderedKeys.Add(strKey);
             base.OnInsertComplete(key, value);
         }
 
-        protected override void OnRemoveComplete(object key, object value)
-        {
+        protected override void OnRemoveComplete(object key, object value) {
             string strKey = key as string;
             orderedKeys.Remove(strKey);
             base.OnRemoveComplete(key, value);
         }
 
-        protected override void OnSetComplete(object key, object oldValue, object newValue)
-        {
+        protected override void OnSetComplete(object key, object oldValue, object newValue) {
             string strKey = key as string;
-            if (!orderedKeys.Contains(strKey))
+            if(!orderedKeys.Contains(strKey))
                 orderedKeys.Add(strKey);
             base.OnSetComplete(key, oldValue, newValue);
         }
 
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return JsonFormatter.Serialize(this);
         }
     }
