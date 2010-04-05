5fc28839c4a6724f77b06b02328ef52f80161055
Craig Wilson <craiggwilson@gmail.com>
2010-04-05 17:29:34 -0500

    adding basic linq objects.

---
+++ MongoDBDriver/Linq/MongoQueryObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MongoDB.Driver.Linq
+{
+    public class MongoQueryObject
+    {
+        public Document Fields { get; set; }
+
+        public int NumberToSkip { get; set; }
+
+        public int NumberToLimit { get; set; }
+
+        public Document Order { get; set; }
+
+        public Document Query { get; set; }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MongoDBDriver/Linq/MongoQueryProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace MongoDB.Driver.Linq
+{
+    public class MongoQueryProvider : IQueryProvider
+    {
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            return new MongoQuery<TElement>(this, expression);
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+            try
+            {
+                return (IQueryable)Activator.CreateInstance(typeof(MongoQuery<>).MakeGenericType(elementType), new object[] { this, expression });
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            return (TResult)Execute(expression);
+        }
+
+        public object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MongoQueryObject GetQueryObject(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
