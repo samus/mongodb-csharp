366d2d9e187b460ecbbd61216c01ceba02987764
Craig Wilson <craiggwilson@gmail.com>
2010-04-19 08:52:40 -0500

    refactored FieldBinder to support more array indexers.

--- MongoDBDriver/Linq/FieldBinder.cs
+++ MongoDBDriver/Linq/FieldBinder.cs
@@ -15,96 +15,131 @@ namespace MongoDB.Driver.Linq
         {
             typeof(ICollection), typeof(ICollection<>)
         };
+
+        private bool _canBeField;
+        private Stack<string> _fieldParts;
+        private FieldFinder _finder;
+
         public Expression Bind(Expression expression)
         {
+            _finder = new FieldFinder();
             return Visit(expression);
         }
 
-        protected override Expression VisitMemberAccess(System.Linq.Expressions.MemberExpression m)
+        protected override Expression Visit(Expression exp)
         {
-            var declaringType = m.Member.DeclaringType;
-            if (!IsNativeToMongo(declaringType) && !IsCollection(declaringType))
-            {
-                var fieldName = GetFieldName(m);
-                if (fieldName != null)
-                    return new FieldExpression(fieldName, m);
-            }
+            if (exp == null)
+                return exp;
 
-            return base.VisitMemberAccess(m);
+            var fieldName = _finder.Find(exp);
+            if (fieldName != null)
+                return new FieldExpression(fieldName, exp);
+
+            return base.Visit(exp);
         }
 
-        protected override Expression VisitMethodCall(MethodCallExpression m)
+        private class FieldFinder : ExpressionVisitor
         {
-            if (m.Method.DeclaringType == typeof(MongoQueryable))
+            private Stack<string> _fieldParts;
+            private bool _isBlocked;
+
+            public string Find(Expression expression)
             {
-                if (m.Method.Name == "Key")
+                _fieldParts = new Stack<string>();
+                _isBlocked = false;
+                Visit(expression);
+                var fieldName = string.Join(".", _fieldParts.ToArray());
+                if (_isBlocked)
+                    fieldName = null;
+
+                return fieldName;
+            }
+
+            protected override Expression Visit(Expression exp)
+            {
+                if (exp == null)
+                    return null;
+
+                switch (exp.NodeType)
                 {
-                    return new FieldExpression((string)((ConstantExpression)m.Arguments[1]).Value, m);
+                    case ExpressionType.Call:
+                    case ExpressionType.MemberAccess:
+                    case ExpressionType.Parameter:
+                        return base.Visit(exp);
+                    default:
+                        _isBlocked = true;
+                        return exp;
                 }
             }
-            else if (typeof(Document).IsAssignableFrom(m.Method.DeclaringType))
+
+            protected override Expression VisitMemberAccess(System.Linq.Expressions.MemberExpression m)
             {
-                if (m.Method.Name == "get_Item") //TODO: does this work for VB?
+                var declaringType = m.Member.DeclaringType;
+                if (!IsNativeToMongo(declaringType) && !IsCollection(declaringType))
                 {
-                    return new FieldExpression((string)((ConstantExpression)m.Arguments[0]).Value, m);
+                    _fieldParts.Push(m.Member.Name);
+                    Visit(m.Expression);
+                    return m;
                 }
+
+                _isBlocked = true;
+                return m;
             }
 
-            return base.VisitMethodCall(m);
-        }
-
-        private static bool CanBeField(Type type)
-        {
-            return true;
-        }
-
-        private static string GetFieldName(MemberExpression m)
-        {
-            var memberNames = new Stack<string>();
-            var p = m;
-            while (p.Expression != null && p.Expression.NodeType == ExpressionType.MemberAccess)
+            protected override Expression VisitMethodCall(MethodCallExpression m)
             {
-                memberNames.Push(p.Member.Name);
-                p = (MemberExpression)p.Expression;
+                if (m.Method.DeclaringType == typeof(MongoQueryable))
+                {
+                    if (m.Method.Name == "Key")
+                    {
+                        _fieldParts.Push((string)((ConstantExpression)m.Arguments[1]).Value);
+                        Visit(m.Arguments[0]);
+                        return m;
+                    }
+                }
+                else if (typeof(Document).IsAssignableFrom(m.Method.DeclaringType))
+                {
+                    if (m.Method.Name == "get_Item") //TODO: does this work for VB?
+                    {
+                        _fieldParts.Push((string)((ConstantExpression)m.Arguments[0]).Value);
+                        Visit(m.Object);
+                        return m;
+                    }
+                }
+
+                _isBlocked = true;
+                return m;
             }
 
-            if (p.Expression != null && p.Expression.NodeType == ExpressionType.Parameter)
+            private static bool IsCollection(Type type)
             {
-                memberNames.Push(p.Member.Name);
-                return string.Join(".", memberNames.ToArray());
+                //HACK: this is going to generally subvert custom objects that implement ICollection or ICollection<T>, 
+                //but are not collections
+                if (type.IsGenericType)
+                    type = type.GetGenericTypeDefinition();
+
+                return _collectionTypes.Any(x => x.IsAssignableFrom(type));
             }
 
-            return null;
-        }
+            private static bool IsNativeToMongo(Type type)
+            {
+                //TODO: this code exists here and in BsonClassMapDescriptor.  Should probably be centralized...
+                var typeCode = Type.GetTypeCode(type);
 
-        private static bool IsCollection(Type type)
-        {
-            //HACK: this is going to generally subvert custom objects that implement ICollection or ICollection<T>, 
-            //but are not collections
-            if (type.IsGenericType)
-                type = type.GetGenericTypeDefinition();
+                if (typeCode != TypeCode.Object)
+                    return true;
 
-            return _collectionTypes.Any(x => x.IsAssignableFrom(type));
-        }
+                if (type == typeof(Guid))
+                    return true;
 
-        private static bool IsNativeToMongo(Type type)
-        {
-            //TODO: this code exists here and in BsonClassMapDescriptor.  Should probably be centralized...
-            var typeCode = Type.GetTypeCode(type);
+                if (type == typeof(Oid))
+                    return true;
 
-            if (typeCode != TypeCode.Object)
-                return true;
+                if (type == typeof(byte[]))
+                    return true;
 
-            if (type == typeof(Guid))
-                return true;
-
-            if (type == typeof(Oid))
-                return true;
-
-            if (type == typeof(byte[]))
-                return true;
-
-            return false;
+                return false;
+            }
         }
     }
 }
