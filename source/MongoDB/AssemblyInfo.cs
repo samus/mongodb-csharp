afde31e47e9315013a050458c13956972030b041
Steve Wagner <lanwin@lanwin.de>
2010-04-27 18:33:38 +0200

    Huge restructuring of the project. Move MongoDB.Driver* to MongoDB. Move source files to source subdirectory. No namespace fixes yet.

--- source/MongoDB/AssemblyInfo.cs
+++ source/MongoDB/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Security.Permissions;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("MongoDB.Driver")]
-[assembly: AssemblyDescription("MongoDB.Driver for C#")]
-[assembly: AssemblyProduct("MongoDB.Driver")]
+[assembly: AssemblyTitle("MongoDB")]
+[assembly: AssemblyDescription("MongoDB-CSharp driver for C#")]
+[assembly: AssemblyProduct("MongoDB-CSharp")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
@@ -20,4 +20,4 @@ using System.Security.Permissions;
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Bson")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
 
-[assembly: InternalsVisibleTo("MongoDB.Driver.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ed9e936c4563336be2e14ca802ea727ff49cad3bb1c0b287beed2a9b5eb823c4c44becc80be4bb11dcd7e49d5d6171f68b488853dcbdeb3152ea3db95ba13a70855a715ee21ac76b67f50bcbc93f2e29e409530a00b98fa79b06ac008dd1f4f3582ba6746af3d218b43b70a63254b094be1a2d493590837273f357fc56b2a7a0")]
+[assembly: InternalsVisibleTo("MongoDB.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ed9e936c4563336be2e14ca802ea727ff49cad3bb1c0b287beed2a9b5eb823c4c44becc80be4bb11dcd7e49d5d6171f68b488853dcbdeb3152ea3db95ba13a70855a715ee21ac76b67f50bcbc93f2e29e409530a00b98fa79b06ac008dd1f4f3582ba6746af3d218b43b70a63254b094be1a2d493590837273f357fc56b2a7a0")]
