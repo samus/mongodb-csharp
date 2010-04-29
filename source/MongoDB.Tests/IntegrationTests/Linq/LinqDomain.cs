9f6c35b3478927f097d17d3f21e533d832ccc67a
Craig Wilson <craiggwilson@gmail.com>
2010-04-29 05:32:40 -0500

    moved linq domain to its own file.

---
+++ source/MongoDB.Tests/IntegrationTests/Linq/LinqDomain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MongoDB.IntegrationTests.Linq
+{
+    public class Person
+    {
+        //[MongoAlias("fn")]
+        public string FirstName { get; set; }
+
+        //[MongoAlias("ln")]
+        public string LastName { get; set; }
+
+        //[MongoAlias("age")]
+        public int Age { get; set; }
+
+        //[MongoAlias("add")]
+        public Address PrimaryAddress { get; set; }
+
+        //[MongoAlias("otherAdds")]
+        public List<Address> Addresses { get; set; }
+
+        //[MongoAlias("emps")]
+        public int[] EmployerIds { get; set; }
+    }
+
+    public class Address
+    {
+        //[MongoAlias("city")]
+        public string City { get; set; }
+    }
+}
