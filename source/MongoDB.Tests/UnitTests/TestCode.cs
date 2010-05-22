177d4c432e16b35276df2c171ab072b422a763f8
Steve Wagner <lanwin@lanwin.de>
2010-05-22 16:32:30 +0200

    Mark Code Serializeable. Fix Code XmlSerialization. Add Code Equals. Add some more Code tests.

--- source/MongoDB.Tests/UnitTests/TestCode.cs
+++ source/MongoDB.Tests/UnitTests/TestCode.cs
@@ -9,6 +9,31 @@ namespace MongoDB.UnitTests
     public class TestCode
     {
         [Test]
+        public void CanBeConstructedWithValue()
+        {
+            var code = new Code("code");
+
+            Assert.AreEqual("code",code.Value);
+        }
+
+        [Test]
+        public void CanBeConstructoredWithNull()
+        {
+            var code = new Code(null);
+
+            Assert.IsNull(code.Value);
+        }
+
+        [Test]
+        public void CanBeEqual()
+        {
+            var code1 = new Code("code");
+            var code2 = new Code("code");
+            
+            Assert.AreEqual(code1,code2);
+        }
+
+        [Test]
         public void CanBeBinarySerialized()
         {
             var source = new Code("code");
@@ -27,6 +52,19 @@ namespace MongoDB.UnitTests
         public void CanBeXmlSerialized()
         {
             var source = new Code("code");
+            var serializer = new XmlSerializer(typeof(Code));
+
+            var writer = new StringWriter();
+            serializer.Serialize(writer, source);
+            var dest = (Code)serializer.Deserialize(new StringReader(writer.ToString()));
+
+            Assert.AreEqual(source, dest);
+        }
+
+        [Test]
+        public void CanBeXmlSerializedWithNullValue()
+        {
+            var source = new Code(null);
             var serializer = new XmlSerializer(typeof(Code));
 
             var writer = new StringWriter();
--- source/MongoDB/Code.cs
+++ source/MongoDB/Code.cs
@@ -1,22 +1,29 @@
+using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 using MongoDB.Util;
 
 namespace MongoDB
 {
     /// <summary>
     /// </summary>
-    public class Code
+    [Serializable]
+    public class Code : IEquatable<Code>, IXmlSerializable
     {
         /// <summary>
         ///   Initializes a new instance of the <see cref = "Code" /> class.
         /// </summary>
-        public Code(){
+        public Code()
+        {
         }
 
         /// <summary>
         ///   Initializes a new instance of the <see cref = "Code" /> class.
         /// </summary>
         /// <param name = "value">The value.</param>
-        public Code(string value){
+        public Code(string value)
+        {
             Value = value;
         }
 
@@ -25,6 +32,20 @@ namespace MongoDB
         /// </summary>
         /// <value>The value.</value>
         public string Value { get; set; }
+
+        /// <summary>
+        ///   Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name = "other">An object to compare with this object.</param>
+        /// <returns>
+        ///   true if the current object is equal to the <paramref name = "other" /> parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(Code other)
+        {
+            if(ReferenceEquals(null, other))
+                return false;
+            return ReferenceEquals(this, other) || Equals(other.Value, Value);
+        }
 
         /// <summary>
         ///   Determines whether the specified <see cref = "System.Object" /> is equal to this instance.
@@ -36,13 +57,35 @@ namespace MongoDB
         /// <exception cref = "T:System.NullReferenceException">
         ///   The <paramref name = "obj" /> parameter is null.
         /// </exception>
-        public override bool Equals(object obj){
-            var other = obj as Code;
-
-            if(other == null)
+        public override bool Equals(object obj)
+        {
+            if(ReferenceEquals(null, obj))
                 return false;
+            if(ReferenceEquals(this, obj))
+                return true;
+            return obj.GetType() == typeof(Code) && Equals((Code)obj);
+        }
 
-            return Value == other.Value;
+        /// <summary>
+        ///   Implements the operator ==.
+        /// </summary>
+        /// <param name = "left">The left.</param>
+        /// <param name = "right">The right.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator ==(Code left, Code right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        ///   Implements the operator !=.
+        /// </summary>
+        /// <param name = "left">The left.</param>
+        /// <param name = "right">The right.</param>
+        /// <returns>The result of the operator.</returns>
+        public static bool operator !=(Code left, Code right)
+        {
+            return !Equals(left, right);
         }
 
         /// <summary>
@@ -51,7 +94,8 @@ namespace MongoDB
         /// <returns>
         ///   A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
         /// </returns>
-        public override int GetHashCode(){
+        public override int GetHashCode()
+        {
             return (Value != null ? Value.GetHashCode() : 0);
         }
 
@@ -61,8 +105,42 @@ namespace MongoDB
         /// <returns>
         ///   A <see cref = "System.String" /> that represents this instance.
         /// </returns>
-        public override string ToString(){
+        public override string ToString()
+        {
             return string.Format(@"{{ ""$code"": ""{0}"" }}", JsonFormatter.Escape(Value));
+        }
+
+        /// <summary>
+        /// This method is reserved and should not be used. When implementing the IXmlSerializable interface, you should return null (Nothing in Visual Basic) from this method, and instead, if specifying a custom schema is required, apply the <see cref="T:System.Xml.Serialization.XmlSchemaProviderAttribute"/> to the class.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Xml.Schema.XmlSchema"/> that describes the XML representation of the object that is produced by the <see cref="M:System.Xml.Serialization.IXmlSerializable.WriteXml(System.Xml.XmlWriter)"/> method and consumed by the <see cref="M:System.Xml.Serialization.IXmlSerializable.ReadXml(System.Xml.XmlReader)"/> method.
+        /// </returns>
+        XmlSchema IXmlSerializable.GetSchema()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Generates an object from its XML representation.
+        /// </summary>
+        /// <param name="reader">The <see cref="T:System.Xml.XmlReader"/> stream from which the object is deserialized.</param>
+        void IXmlSerializable.ReadXml(XmlReader reader)
+        {
+            if(reader.IsEmptyElement)
+                return;
+
+            Value = reader.ReadString();
+        }
+
+        /// <summary>
+        /// Converts an object into its XML representation.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Xml.XmlWriter"/> stream to which the object is serialized.</param>
+        void IXmlSerializable.WriteXml(XmlWriter writer)
+        {
+            if(Value != null)
+                writer.WriteString(Value);
         }
     }
 }
