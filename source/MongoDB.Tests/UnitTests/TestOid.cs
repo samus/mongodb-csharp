3194b430b3e87b6d6665918daa774e7aa8a2006b
Steve Wagner <lanwin@lanwin.de>
2010-05-22 11:33:39 +0200

    Cleanup OidTests.

--- source/MongoDB.Tests/UnitTests/TestOid.cs
+++ source/MongoDB.Tests/UnitTests/TestOid.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
 using NUnit.Framework;
 
 namespace MongoDB.UnitTests
@@ -10,181 +9,113 @@ namespace MongoDB.UnitTests
     public class TestOid
     {
         [Test]
-        public void TestIDLength(){
-            bool thrown = false;
-            try{
-                new Oid("BAD0");
-            }catch(Exception){
+        public void TestCtor()
+        {
+            var thrown = false;
+            try
+            {
+                new Oid("4a7067c30a57000000008ecb");
+            }
+            catch(ArgumentException)
+            {
                 thrown = true;
             }
-            Assert.IsTrue(thrown,"No length exception thrown");
-        }
-        
-        [Test]
-        public void TestIDCharacters(){
-            bool thrown = false;
-            try{
-                new Oid("BADBOYc30a57000000008ecb");
-            }catch(Exception){
-                thrown = true;
-            }
-            Assert.IsTrue(thrown,"No invalid characters exception thrown");         
-        }
-        
-        [Test]
-        public void TestQuoteCharacters(){
-            Oid val = new Oid(@"""4a7067c30a57000000008ecb""");
-            try{
-                new Oid(val.ToString());
-            }catch(ArgumentException){
-                Assert.Fail("Creating an Oid from the json representation should not fail.");
-            }
-        }
-        
-        [Test]
-        public void TestOidEquality(){
-            Oid val = new Oid("4a7067c30a57000000008ecb");
-            Oid other = new Oid("4a7067c30a57000000008ecb");
-            
-            Assert.IsTrue(val.Equals(other), "Equals(Oid) did not work");
-            Assert.IsTrue(val == other, "== operator did not work");
-            Assert.IsTrue(val == val, "Objects should be equal to itself.");
-            
-        }
-                        
-        [Test]
-        public void TestOidInEquality(){
-            Oid val = new Oid("4a7067c30a57000000008ecb");
-            Oid other = new Oid("5a7067c30a57000000008ecb");
-            Oid nilo = null;
-            
-            Assert.IsFalse(val == null);
-            Assert.IsFalse(nilo == val);
-            Assert.IsFalse(val == nilo);
-            Assert.IsFalse(val == other);
-            Assert.IsFalse(val.Equals(other));
-            Assert.IsTrue(val != null);
-            Assert.IsTrue(val != other);
-            
-        }
-        
-        [Test]
-        public void TestOidEqualityToNull(){
-            Oid val = Oid.NewOid();
-            Oid other = null;
-            Oid other2 = null;
-            Assert.AreNotEqual(val, other);
-            Assert.AreNotEqual(other, val);
-            Assert.IsTrue(other == other2);
+            Assert.IsFalse(thrown, "ID should be fine.");
         }
 
         [Test]
-        public void TestOidComparisons(){
-            Oid lower = new Oid("4a7067c30a57000000008ecb");
-            Oid higher = new Oid("5a7067c30a57000000008ecb");
-            
-            Assert.AreEqual(1, lower.CompareTo(null));
-            Assert.AreEqual(1, higher.CompareTo(lower));
-            
-            Assert.IsTrue(lower < higher);
-            Assert.IsTrue(higher > lower);
+        public void TestDate()
+        {
+            const string hex = "4B458B95D114BE541B000000";
+            var oid = new Oid(hex);
+            //Expected: 2010-01-07 02:24:56.633
+            var expected = new DateTime(2010, 1, 7, 7, 21, 57, DateTimeKind.Utc);
+            Assert.AreEqual(expected, oid.Created);
         }
-        
+
         [Test]
-        public void TestOidFromBytes(){
-            byte[] bytes = new byte[]{1,2,3,4,5,6,7,8,9,10,11,12};
-            string hex = "0102030405060708090a0b0c";
-            
-            Oid bval = new Oid(bytes);
-            Oid sval = new Oid(hex);
-            Assert.AreEqual(bval, sval);
-        }
-        
-        [Test]
-        public void TestNullValue(){
-            bool thrown = false;
-            try{
-                new Oid(String.Empty);
-            }catch(Exception){
-                thrown = true;
-            }
-            Assert.IsTrue(thrown,"Null value exception not thrown");            
-        }       
-        
-        [Test]
-        public void TestCtor(){     
-            bool thrown = false;
-            try{
-                new Oid("4a7067c30a57000000008ecb");
-            }catch(ArgumentException){
-                thrown = true;
-            }
-            Assert.IsFalse(thrown,"ID should be fine.");                        
-        }
-        
-        [Test]
-        public void TestDecode(){
-            string hex = "4a7067c30a57000000008ecb";
-            Oid oid = new Oid(hex);
-            
+        public void TestDecode()
+        {
+            const string hex = "4a7067c30a57000000008ecb";
+            var oid = new Oid(hex);
+
             Assert.AreEqual(hex, oid.ToString());
+        }
+
+        [Test]
+        public void TestEquals()
+        {
+            const string hex = "4a7067c30a57000000008ecb";
+            Assert.AreEqual(new Oid(hex), new Oid(hex));
         }
 
         [Test]
         public void TestFormatJ()
         {
-            var hex = "4a7067c30a57000000008ecb";
+            const string hex = "4a7067c30a57000000008ecb";
             var oid = new Oid(hex);
 
             Assert.AreEqual("\"" + hex + "\"", oid.ToString("J"));
         }
 
         [Test]
-        public void TestEquals(){
-            string hex = "4a7067c30a57000000008ecb";
-            Assert.AreEqual(new Oid(hex), new Oid(hex));
-            
+        public void TestIDCharacters()
+        {
+            var thrown = false;
+            try
+            {
+                new Oid("BADBOYc30a57000000008ecb");
+            }
+            catch(Exception)
+            {
+                thrown = true;
+            }
+            Assert.IsTrue(thrown, "No invalid characters exception thrown");
         }
-        [Test]
-        public void TestNotEquals(){
-            string hex = "4a7067c30a57000000008ecb";
-            string hex2 = "4a7067c30a57000000008ecc";
-            Assert.AreNotEqual(new Oid(hex), new Oid(hex2));
-            
-        }
-        
-        [Test]
-        public void TestDate(){
-            string hex = "4B458B95D114BE541B000000";
-            Oid oid = new Oid(hex);
-            //Expected: 2010-01-07 02:24:56.633
-            DateTime expected = new DateTime(2010,1,7,7,21,57,DateTimeKind.Utc);
-            Assert.AreEqual(expected,oid.Created);
-        }
-        
-        [Test]
-        public void TestToByteArray(){
-            byte[] bytes = new byte[]{1,2,3,4,5,6,7,8,9,10,11,12};
-
-            Oid bval = new Oid(bytes);
-            byte[] bytes2 = bval.ToByteArray();
-            
-            Assert.IsNotNull(bytes2);
-            Assert.AreEqual(12, bytes2.Length);
-            Assert.AreEqual(bytes, bytes2);
-        }        
 
         [Test]
-        public void TestNewOidFromToString(){
-            var hex = "4B458B95D114BE541B000000";
+        public void TestIDLength()
+        {
+            var thrown = false;
+            try
+            {
+                new Oid("BAD0");
+            }
+            catch(Exception)
+            {
+                thrown = true;
+            }
+            Assert.IsTrue(thrown, "No length exception thrown");
+        }
+
+        [Test]
+        public void TestNewOidFromToString()
+        {
+            const string hex = "4B458B95D114BE541B000000";
             var firstOid = new Oid(hex);
             var secondOid = new Oid(firstOid.ToString());
-            
+
             Assert.AreEqual(firstOid.ToString(), secondOid.ToString());
         }
 
         [Test]
-        public void TestOidCanBeSerialized(){
+        public void TestNotEquals()
+        {
+            const string hex = "4a7067c30a57000000008ecb";
+            const string hex2 = "4a7067c30a57000000008ecc";
+            Assert.AreNotEqual(new Oid(hex), new Oid(hex2));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullValue()
+        {
+            new Oid(String.Empty);
+        }
+
+        [Test]
+        public void TestOidCanBeSerialized()
+        {
             var serializer = new BinaryFormatter();
 
             var oidSource = Oid.NewOid();
@@ -196,7 +127,96 @@ namespace MongoDB.UnitTests
                 oidDesc = (Oid)serializer.Deserialize(mem);
             }
 
-            Assert.AreEqual(oidSource,oidDesc);
+            Assert.AreEqual(oidSource, oidDesc);
+        }
+
+        [Test]
+        public void TestOidComparisons()
+        {
+            var lower = new Oid("4a7067c30a57000000008ecb");
+            var higher = new Oid("5a7067c30a57000000008ecb");
+
+            Assert.AreEqual(1, lower.CompareTo(null));
+            Assert.AreEqual(1, higher.CompareTo(lower));
+
+            Assert.IsTrue(lower < higher);
+            Assert.IsTrue(higher > lower);
+        }
+
+        [Test]
+        public void TestOidEquality()
+        {
+            var val = new Oid("4a7067c30a57000000008ecb");
+            var other = new Oid("4a7067c30a57000000008ecb");
+
+            Assert.IsTrue(val.Equals(other), "Equals(Oid) did not work");
+            Assert.IsTrue(val == other, "== operator did not work");
+            Assert.IsTrue(val == val, "Objects should be equal to itself.");
+        }
+
+        [Test]
+        public void TestOidEqualityToNull()
+        {
+            var val = Oid.NewOid();
+            Oid other = null;
+            Oid other2 = null;
+            Assert.AreNotEqual(val, other);
+            Assert.AreNotEqual(other, val);
+            Assert.IsTrue(other == other2);
+        }
+
+        [Test]
+        public void TestOidFromBytes()
+        {
+            var bytes = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+            var hex = "0102030405060708090a0b0c";
+
+            var bval = new Oid(bytes);
+            var sval = new Oid(hex);
+            Assert.AreEqual(bval, sval);
+        }
+
+        [Test]
+        public void TestOidInEquality()
+        {
+            var val = new Oid("4a7067c30a57000000008ecb");
+            var other = new Oid("5a7067c30a57000000008ecb");
+            Oid nilo = null;
+
+            Assert.IsFalse(val == null);
+            Assert.IsFalse(nilo == val);
+            Assert.IsFalse(val == nilo);
+            Assert.IsFalse(val == other);
+            Assert.IsFalse(val.Equals(other));
+            Assert.IsTrue(val != null);
+            Assert.IsTrue(val != other);
+        }
+
+        [Test]
+        public void TestQuoteCharacters()
+        {
+            var val = new Oid(@"""4a7067c30a57000000008ecb""");
+            try
+            {
+                new Oid(val.ToString());
+            }
+            catch(ArgumentException)
+            {
+                Assert.Fail("Creating an Oid from the json representation should not fail.");
+            }
+        }
+
+        [Test]
+        public void TestToByteArray()
+        {
+            var bytes = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+            var bval = new Oid(bytes);
+            var bytes2 = bval.ToByteArray();
+
+            Assert.IsNotNull(bytes2);
+            Assert.AreEqual(12, bytes2.Length);
+            Assert.AreEqual(bytes, bytes2);
         }
     }
 }
